79d52e76a9f7beed446f5f6f8b6374f4a86088e2
marinademinescu <demi.marina@yahoo.it>
2025-02-20 19:26:20 +0100

    es-1-react-vite

--- src/App.jsx
+++ src/App.jsx
@@ -4,31 +4,15 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+ 
   return (
-    <>
+  
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h1>React con Vite!</h1>
+      <p>Questa è la mia prima app con Vite!</p>
       </div>
-      <h1>titolo</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count  {count}
-        </button>
-        <p>
-          Edit edit edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      
+    
   )
 }
 
