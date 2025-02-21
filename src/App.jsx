1860ca3201025190f2f42e81defbd5d0a02d555c
marinademinescu <demi.marina@yahoo.it>
2025-02-21 10:58:44 +0100

    file-CIaoMondo-HelloWorld

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import HelloWorld from './CiaoMondo';
+
+
+
 
 function App() {
  
   return (
   
       <div>
+           <HelloWorld />
       <h1>React con Vite!</h1>
       <p>Questa è la mia prima app con Vite</p>
       </div>
