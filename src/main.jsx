e0b3f1bc2d391f7a401a153e243201e12c1e23fb
marinademinescu <demi.marina@yahoo.it>
2025-03-11 17:04:19 +0100

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import ReactDom from 'react-dom/client'
 import App from './App.jsx'
-import ListProvider from './components/providers/ListProvider.jsx'
+import { BrowserRouter } from 'react-router-dom'
 
-createRoot(document.getElementById('root')).render(
- <ListProvider >
+ReactDom.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
     <App />
-  </ListProvider>
+  </BrowserRouter>
 )
