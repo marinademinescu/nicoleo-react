ca41191de109a054b195b18756e478d0f38eddc0
marinademinescu <demi.marina@yahoo.it>
2025-03-12 12:47:26 +0100

    aboutbutton

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,19 @@
+import { useNavigate } from "react-router-dom"
 
 const About = () => {
+
+    const navigate = useNavigate()
+
+    const handleBacktoHome = () => {
+
+        navigate("/")
+    }
     return (
         <div>
             <h1>ABOUT</h1>
             <h2>Informazioni sull'applicazione</h2>
             <p>Questa applicazione ti permette di gestire una lista di todo</p>
+            <button onClick={handleBacktoHome}>back to homepage</button>
         </div>
     )
 }
