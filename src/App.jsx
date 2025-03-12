1f95ba655a9000ed8b482f0db46287ba10d3647e
marinademinescu <demi.marina@yahoo.it>
2025-03-12 11:48:25 +0100

    apphomeabout

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,24 @@
-import { useContext } from 'react';
-import './App.css';
-import ToDoListInput from './components/ToDoListInput/ToDoListInput';
-import ListProvider from './components/providers/ListProvider';
+import Navbar from "./components/Navbar";
+import {BrowserRouter,  Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Layout from "./layouts/Layout";
+
 
 
 function App() {
 
   return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes >
+        <Route path="/" element={<Layout />}>
+          <Route path="" element={<Home />} />
+          <Route path="about" element={<About />} />
+        </Route>
+      </Routes>
 
-    <ListProvider>
-      <ToDoListInput />
-    </ListProvider>
-
+    </BrowserRouter>
   )
 
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
-import ReactDom from 'react-dom/client'
+import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
 
-ReactDom.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+
+createRoot(document.getElementById('root')).render(
+  
     <App />
-  </BrowserRouter>
+  
 )
